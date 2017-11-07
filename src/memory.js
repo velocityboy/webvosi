fcbfd75ad3837bf63eb0d6f0d40cdcc54e50c69c
Jim Geist <jimge@rodentia.net>
2017-11-06 19:02:49 -0800

    Start some tests

--- src/memory.js
+++ src/memory.js
@@ -1,1 +1,21 @@
 // @flow
+
+export default class Memory {
+  _memory: Uint8Array;
+
+  constructor() {
+    this._memory = new Uint8Array(0x10000);
+  }
+
+  initializeRegion(data: Uint8Array, address: number): void {
+    this._memory.set(data, address);
+  }
+
+  readByte(address: number): number {
+    return this._memory[address];
+  }
+
+  writeByte(address: number, data: number): void {
+    this._memory[address] = data;
+  }
+}
---
+++ test/testMemory.js
@@ -0,0 +1,27 @@
+var assert = require('assert');
+var Memory = require('../out/Memory').default;
+
+var memory;
+
+beforeEach(function() {
+  this.memory = new Memory();
+})
+
+describe('Memory', function() {
+  describe('#readByte', function() {
+    it('should read back uninitialized memory as zero', function() {
+      assert.equal(this.memory.readByte(0), 0)
+    })
+    it('should read back what was written', function() {
+      this.memory.writeByte(0x1000, 42);
+      assert.equal(this.memory.readByte(0x1000), 42);
+    })
+    it ('should read back initialized memory', function() {
+      let data = new Uint8Array([1,2,3]);
+      this.memory.initializeRegion(data, 0x1000);
+      assert.equal(this.memory.readByte(0x1000), 1);
+      assert.equal(this.memory.readByte(0x1001), 2);
+      assert.equal(this.memory.readByte(0x1002), 3);
+    })
+  })
+})
