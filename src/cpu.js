777e23fc2117300d1e8eb9c8a0878d40f3537d9b
Jim Geist <jimge@rodentia.net>
2017-11-07 20:00:11 -0800

    BEQ

--- src/cpu.js
+++ src/cpu.js
@@ -70,6 +70,7 @@ export default class CPU {
     this._dispatch[0x7D] = () => this._adc(this._absoluteX(), 4);
     this._dispatch[0x90] = this._bcc.bind(this);
     this._dispatch[0xB0] = this._bcs.bind(this);
+    this._dispatch[0xF0] = this._beq.bind(this);
 
 
     this.reset();
@@ -168,6 +169,19 @@ export default class CPU {
     const offset = CPU._byteToSigned(this._memory.readByte(this.ip));
 
     if ((this.flags & Flags.C) !== 0) {
+      this.ip = (this.ip + 1 + offset) & 0xFFFF;
+      this._cycles += 3;
+      return;
+    }
+
+    this.ip = CPU._inc16(this.ip);
+    this._cycles += 2;
+  }
+
+  _beq(): void {
+    const offset = CPU._byteToSigned(this._memory.readByte(this.ip));
+
+    if ((this.flags & Flags.Z) !== 0) {
       this.ip = (this.ip + 1 + offset) & 0xFFFF;
       this._cycles += 3;
       return;
